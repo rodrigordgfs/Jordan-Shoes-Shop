a86b89b9d669d06c50a55808750a014a4063d90b
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-07-05 12:05:11 -0300

    Removed API communication temporally

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 // bivewe1009@advew.com = email api
 
 let backToTop = document.getElementById("back_to_top");
-let shoesList = document.getElementById("main__content__list");
+// let shoesList = document.getElementById("main__content__list");
 
-async function getAPIData() {
-  try {
-    const response = await fetch(
-      "https://60e2c2cf9103bd0017b47526.mockapi.io/api/v1/shoes"
-    );
-    return await response.json();
-  } catch (error) {
-    throw new Error(error);
-  }
-}
+// async function getAPIData() {
+//   try {
+//     const response = await fetch(
+//       "https://60e2c2cf9103bd0017b47526.mockapi.io/api/v1/shoes"
+//     );
+//     return await response.json();
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// }
 
-function buildList(data) {
-  for (const item of data) {
-    const overlayText = document.createElement("p");
-    overlayText.innerHTML = item.name;
-    const divtText = document.createElement("div");
-    divtText.classList.add("main__content__list_item__text");
-    divtText.appendChild(overlayText);
-    const overlay = document.createElement("div");
-    overlay.classList.add("main__content__list_item__overlay");
-    overlay.appendChild(divtText);
-    const cardImage = document.createElement("img");
-    cardImage.src = item.image;
-    cardImage.alt = item.name;
-    const itemList = document.createElement("li");
-    itemList.classList.add("main__content__list_item");
-    itemList.appendChild(cardImage);
-    itemList.appendChild(overlay);
-    shoesList.appendChild(itemList);
-  }
-}
+// function buildList(data) {
+//   for (const item of data) {
+//     const overlayText = document.createElement("p");
+//     overlayText.innerHTML = item.name;
+//     const divtText = document.createElement("div");
+//     divtText.classList.add("main__content__list_item__text");
+//     divtText.appendChild(overlayText);
+//     const overlay = document.createElement("div");
+//     overlay.classList.add("main__content__list_item__overlay");
+//     overlay.appendChild(divtText);
+//     const cardImage = document.createElement("img");
+//     cardImage.src = item.image;
+//     cardImage.alt = item.name;
+//     const itemList = document.createElement("li");
+//     itemList.classList.add("main__content__list_item");
+//     itemList.appendChild(cardImage);
+//     itemList.appendChild(overlay);
+//     shoesList.appendChild(itemList);
+//   }
+// }
 
-getAPIData()
-  .then((data) => {
-    buildList(data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// getAPIData()
+//   .then((data) => {
+//     buildList(data);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
 // Show/Hide Back to top button
 window.addEventListener("scroll", () => {
